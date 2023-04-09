fb91101c88842547df66075c87fecf8a717410bc
Дмитрий Балабанов <dmittrj@outlook.com>
2023-04-09 22:04:57 +0300

    Hello commit

---
+++ main.sql
@@ -0,0 +1,6 @@
+/* Creating db */
+IF DB_ID (N'MobileOperator_by_DmitryBalabanov') IS NOT NULL
+DROP DATABASE MobileOperator_by_DmitryBalabanov;
+
+CREATE DATABASE MobileOperator_by_DmitryBalabanov
+GO
