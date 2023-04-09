37ff7dd197c49757eb84601dfa105a6988b9d80b
Дмитрий Балабанов <dmittrj@outlook.com>
2023-04-09 23:45:13 +0300

    Query to fill database

---
+++ fill.sql
@@ -0,0 +1,7 @@
+USE MobileOperator_by_DmitryBalabanov;
+
+INSERT INTO [Passport] VALUES ('2398', '129833', 'ГУ МВД России', '2000/09/11', '500-111', '1980/08/17');
+INSERT INTO [Passport] VALUES ('8567', '897644', 'ГУ МВД России', '2002/03/30', '500-111', '1982/03/7');
+
+INSERT INTO [Subscriber] VALUES ('Голый Андрей Иванович', '2398', '129833', '2023/01/06', 3)
+INSERT INTO [Subscriber] VALUES ('Гаражный Денис Витальевич', '8567', '897644', '2022/12/29', 2)
