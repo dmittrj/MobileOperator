a6232734b6bff2b328c0ccfcdb971681dc0be4fa
Дмитрий Балабанов <dmittrj@outlook.com>
2023-04-09 23:05:38 +0300

    Passport table added

--- main.sql
+++ main.sql
@@ -22,6 +22,20 @@ CREATE TABLE Subscriber (
 	[sub_tariff] INT NULL /* current tariff */
 );
 
+
+/* Passport entity */
+DROP TABLE IF EXISTS [Passport];
+CREATE TABLE Passport (
+	[pss_series] INT NOT NULL, /* Passport series */
+	[pss_number] INT NOT NULL, /* Passport number */
+	[pss_issued_by] NVARCHAR(64) NOT NULL, /* Who issued the passport */
+	[pss_issued_date] DATE NOT NULL, /* When the passport was issued */
+	[pss_division_code] VARCHAR(10) NOT NULL, /* The code of the organization that issued the passport */
+	[pss_date_of_birth] DATE NOT NULL, /* The date of birth of the passport holder */
+	PRIMARY KEY ([pss_series], [pss_number])
+);
+
+
 /* Tariff entity */
 DROP TABLE IF EXISTS [Tariff];
 CREATE TABLE Tariff (
