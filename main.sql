a9917e70ede6c3286c530e038dca6495bf2aec07
Дмитрий Балабанов <dmittrj@outlook.com>
2023-04-09 22:34:18 +0300

    Subscriber table added

--- main.sql
+++ main.sql
@@ -1,6 +1,22 @@
 /* Creating db */
+USE master;
 IF DB_ID (N'MobileOperator_by_DmitryBalabanov') IS NOT NULL
-DROP DATABASE MobileOperator_by_DmitryBalabanov;
+	DROP DATABASE MobileOperator_by_DmitryBalabanov;
 
 CREATE DATABASE MobileOperator_by_DmitryBalabanov
 GO
+
+
+USE MobileOperator_by_DmitryBalabanov;
+
+
+/* Subscriber entity */
+CREATE TABLE Subscriber (
+	sub_id INT IDENTITY(1,1) PRIMARY KEY, /* id */
+
+	sub_name NVARCHAR(64) NOT NULL, /* surname, name, patronymic */
+	sub_passport_id INT NOT NULL, /* link to: PASSPORT */
+	
+	sub_joining_date DATE NULL, /* start date of using the operator's services */
+	sub_tariff INT NULL /* current tariff */
+);
