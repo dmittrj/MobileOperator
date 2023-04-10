4c21bdbeef2b3718ac16084637d68eba1f2205ad
Дмитрий Балабанов <dmittrj@outlook.com>
2023-04-10 10:59:28 +0300

    Sellings table added

--- main.sql
+++ main.sql
@@ -10,6 +10,7 @@ GO
 USE MobileOperator_by_DmitryBalabanov;
 
 
+
 /* Subscriber entity */
 DROP TABLE IF EXISTS [Subscriber];
 CREATE TABLE Subscriber (
@@ -24,6 +25,7 @@ CREATE TABLE Subscriber (
 );
 
 
+
 /* Passport entity */
 DROP TABLE IF EXISTS [Passport];
 CREATE TABLE Passport (
@@ -35,8 +37,8 @@ CREATE TABLE Passport (
 	[ppt_date_of_birth] DATE NOT NULL, /* The date of birth of the passport holder */
 	CONSTRAINT ppt_ser_num PRIMARY KEY CLUSTERED ([ppt_series], [ppt_number])
 );
+ALTER TABLE [Subscriber] ADD CONSTRAINT fk_sub_pptHolds FOREIGN KEY ([sub_passport_s], [sub_passport_n]) REFERENCES Passport([ppt_series], [ppt_number]);
 
-ALTER TABLE [Subscriber] ADD CONSTRAINT sub_pptHolds FOREIGN KEY ([sub_passport_s], [sub_passport_n]) REFERENCES Passport([ppt_series], [ppt_number]);
 
 
 /* Tariff entity */
@@ -48,4 +50,29 @@ CREATE TABLE Tariff (
 	[tar_minutes] INT NOT NULL, /* Minutes to talk-talk */
 	[tar_sms] INT NOT NULL, /* Number of SMS */
 	[tar_internet] REAL NOT NULL /* Amount of Internet traffic, gb */
+);
+
+
+
+/* Package entity */
+DROP TABLE IF EXISTS [Package];
+CREATE TABLE Package (
+	[pak_id] INT IDENTITY(1,1) PRIMARY KEY, /* id */
+
+	[pak_minutes] INT NOT NULL, /* Minutes to talk-talk */
+	[pak_sms] INT NOT NULL, /* Number of SMS */
+	[pak_internet] REAL NOT NULL /* Amount of Internet traffic, gb */
+);
+
+
+
+/* Sellings entity */
+DROP TABLE IF EXISTS [Sellings];
+CREATE TABLE Sellings (
+	[sll_id] INT IDENTITY(1,1) PRIMARY KEY, /* id */
+
+	[sll_sub_id] INT NOT NULL, /* subscriber who purchased a tariff */
+	[sll_tar_id] INT NOT NULL, /* tariff that was purchased */
+	
+	[sll_date] DATETIME NULL, /* date and time when tariff was purchased */
 );
