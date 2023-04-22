c905896da82fa1455b024c4c61b7f8a17184aad2
dmittrj <dmittrj@outlook.com>
2023-04-22 09:09:58 +0300

    New: sorting tariffs according to popularity

--- queries.sql
+++ queries.sql
@@ -9,6 +9,14 @@ FROM Subscriber
 GO
 
 
+-- Sorting tariffs according to popularity
+SELECT sub_tariff AS [Tariff Name], COUNT(sub_tariff) AS [Number of subscribers],
+	(COUNT(sub_tariff) * 100 / (SELECT COUNT(*) FROM Subscriber)) AS [Percentage]
+FROM Subscriber
+GROUP BY sub_tariff
+ORDER BY [Number of subscribers] DESC
+
+
 -- View subscribers’ passport data
 SELECT sub_phone_number, sub_name, ppt_series_number, ppt_issued_by, ppt_issued_date, ppt_division_code
 FROM Subscriber
